efc2ade3d56076e51df1c8f7ed07a3ed0b9ee1f0
kamil <kamil.stawniak@gmx.de>
2025-07-14 11:24:58 +0200

    ssss

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String[] cars = {"BMW", "Audi", "Mazda"};
+        String[] cars = {"BMW", "Audi", "Mazda", "Mazda"};
 
 
         for(String car : cars){
